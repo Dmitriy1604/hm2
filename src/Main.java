e34674164e43c7be47b10a6fa4c857f9c4015123
Dmitriy1604 <Dimon312-91@mail.ru>
2022-07-11 22:50:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+
+        //Задача 1
+
+        byte car = 5;
+        short magazine = 3;
+        int mustang = 2;
+        long pen = 15;
+        float weight = 3.5f;
+        double temperature = 36.6;
+        char globe = 16;
+        boolean oneCar = car > 3;
+
+
+        //Задача 2
+
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightOfAllBoxers = boxer1 + boxer2;
+        double weightDifference = boxer1 - boxer2;
+        System.out.println("общий вес боксеров " + weightOfAllBoxers);
+        System.out.println("разница веса между бойцами " + weightDifference);
+
+
+
+         // задача 3
+
+float bananas = 5;
+float milk = 200;
+float iceCreamSundae = 2;
+float rawEggs = 4;
+float onePortionBananas = 5 * 80;
+float onePortionMilk = 105 * 2;
+float onePortionCreamSundae = 2 *100;
+float onePortionRawEggs = 4 * 70;
+        System.out.println("одна порция в граммах бананы " + onePortionBananas + "грамм " );
+        System.out.println("одна порция молока грамм " + onePortionMilk + "грамм " );
+        System.out.println("одна порция мороженного в грамм " + onePortionCreamSundae + "грамм " );
+        System.out.println(" одна порция ырых яиц в грамм " + onePortionRawEggs + "грамм " );
+        float totalWeight = onePortionBananas + onePortionMilk + onePortionCreamSundae + onePortionRawEggs;
+        System.out.println(" общий вес завтрака в граммах " + totalWeight + "грамм! ");
+        double fromGramsToKilograms = 1090.0 / 1000;
+        System.out.println(" перевод из грамм в килограмм " + fromGramsToKilograms + "килограмм " );
+
+
+
+        // Задача 4
+
+        short fromGramsToKilograms2 = 7 * 1000;
+        System.out.println("перевод из килограмм в грамм " + fromGramsToKilograms2 + "грамм " );
+byte if250 = 7000 / 250;
+        System.out.println("если худеть на 250 грамм в день " + if250 + "дней! ");
+        byte if500 = 7000 / 500;
+        System.out.println("если худеть на 500 грамм в день " + if500 + "дней " );
+        int totalNumberToDays = if250 + if500;
+        System.out.println("общее количество дней похудания для всех дней при всех 250 и 500 " + totalNumberToDays + "дней ");
+int averageNumberOfDays = 42 / 2;
+        System.out.println("среднее количество дней для похудания для двух диет " + averageNumberOfDays + "дней ");
+
+
+
+        //Задача 5
+        //после повышения зарплаты
+        int masha = (int) (67760 * 0.1);
+        System.out.println(" увеличение зарплаты на 10% " + masha + "рублей ");
+        int masha1 = 67760 + 6776;
+        System.out.println("увеличение зарлаты в месяц " + masha1+ "рублей " );
+        int masha3 = 67760 * 12;
+        System.out.println(" заработок маши до повышения за год " + masha3 + "рублей " );
+        int masha4 = 74536 * 12;
+        System.out.println(" заработок маши после повышения зарплаты за год " + masha4 + "рублей " );
+        int masha5 = 894432 - 813120;
+        System.out.println("разница после повышения зарплаты на 10% в год " + masha5 + "рублей "  );
+        int denis = (int) (83690 * 0.1);
+        System.out.println(" увеличение зарплаты на 10% " + denis + "рублей ");
+        int denis1 = 83690 + 8369;
+        System.out.println("увеличение зарлаты в месяц " + denis1+ "рублей " );
+        int denis3 = 83690 * 12;
+        System.out.println(" заработок маши до повышения за год " + denis3 + "рублей " );
+        int denis4 = 92059 * 12;
+        System.out.println(" заработок маши после повышения зарплаты за год " + denis4 + "рублей " );
+        int denis5 = 1104708 - 1004280;
+        System.out.println("разница после повышения зарплаты на 10% в год " + denis5 + "рублей "  );
+        int kristina = (int) (76230 * 0.1);
+        System.out.println(" увеличение зарплаты на 10% " + kristina + "рублей ");
+        int kristina1 = 76230 + 7623;
+        System.out.println("увеличение зарлаты в месяц " + kristina1+ "рублей " );
+        int kristina3 = 76230 * 12;
+        System.out.println(" заработок маши до повышения за год " + kristina3 + "рублей " );
+        int kristina4 = 83853 * 12;
+        System.out.println(" заработок маши после повышения зарплаты за год " + kristina4 + "рублей " );
+        int kristina5 = 1006236 - 914760;
+        System.out.println("разница после повышения зарплаты на 10% в год " + kristina5 + "рублей "  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
